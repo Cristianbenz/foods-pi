f17274bd106a3a3d8383d647d5fd8cb942db7797
Cristianbenz <contactocbenitez@gmail.com>
2022-09-10 12:29:39 -0300

    primer commit a repo

---
+++ api/tests/models/recipe.spec.js
@@ -0,0 +1,13 @@
+const { Recipe, conn } = require('../../src/db.js');
+const { expect } = require('chai');
+
+describe('Recipe model', () => {
+  before(() => conn.authenticate()
+    .catch((err) => {
+      console.error('Unable to connect to the database:', err);
+    }));
+  describe('Validators', () => {
+    beforeEach(() => Recipe.sync({ force: true }));
+    
+  });
+});
