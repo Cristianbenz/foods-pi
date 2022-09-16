9ab399a275f05df9b241d510202c548fa746e1ec
Cristianbenz <contactocbenitez@gmail.com>
2022-09-16 13:29:22 -0300

    hago paginacion

--- client/src/components/pagination/index.jsx
+++ client/src/components/pagination/index.jsx
@@ -1,17 +1,26 @@
-import { useEffect } from "react"
-import { useSelector } from "react-redux"
-import { pagination } from "../../redux/actions"
+import { useState, useEffect } from "react"
 
-export default function Pagination() {
-	const [pag, setPag] = useEffect(0)
-	const recipesArr = useSelector(state => state.recipes)
+export default function Pagination({totalPages=1, handle}) {
+	const [pags, setPags] = useState([])
+	const [currentPage, setCurrentPage] = useState(1)
+
+
+	for (let i = 1; i <= totalPages; i++) {
+		setPags(prevPags => [...prevPags, i])
+	}
+
+	useEffect(() => {
+		handle(currentPage)
+	}, [currentPage, handle])
 
 	return(
 		<div>
 			<span></span>
 				<ul>
 					{
-
+						pags.map(num => {
+							return <li onClick={() => setCurrentPage(num)} key={num}>num</li>
+						})
 					}
 				</ul>
 			<span></span>
--- client/src/sections/home/index.jsx
+++ client/src/sections/home/index.jsx
@@ -1,15 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from 'react-redux'
 import { getRecipes } from "../../redux/actions"
 
 import CardsList from "../../components/cardsList"
 import Filter from "../../components/filter"
+import Pagination from "../../components/pagination"
 
 import { BodyContainer } from './styles'
 
 export default function Home() {
 	const dispatch = useDispatch()
 	const list = useSelector(state => state.recipes)
+	const totalPages = list.length / 9
+	const [recipes, setRecipes] = useState([])
+
+	function handlePagination(currentPage) {
+		setRecipes(list.slice(currentPage - 1 * 9 , currentPage * 8))
+	}
+	
 	useEffect(() => {
 		dispatch(getRecipes())
 	}, [dispatch])
@@ -19,7 +27,11 @@ export default function Home() {
 			<h1 className="title">Recetas</h1>
 			<BodyContainer>
 				<Filter />
-				<CardsList list={list} />
+				<div>
+				<Pagination totalPages={totalPages} handle={handlePagination} />
+				<CardsList list={recipes} />
+				<Pagination totalPages={totalPages} handle={handlePagination} />
+				</div>
 			</BodyContainer>
 		</>
 	)
